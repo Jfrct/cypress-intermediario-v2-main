903e84490827f725d221509e3a5da08b8bd2a017
Jfrc <felipecestaro2011@hotmail.com>
2025-06-19 21:31:26 -0300

    API

--- cypress/e2e/gui/createissue.cy.js
+++ cypress/e2e/gui/createissue.cy.js
@@ -1,28 +1,31 @@
 import { faker } from '@faker-js/faker'
 
-const issue = {
-    title: `issue-${faker.datatype.uuid()}`,
-    description: faker.random.words(5),
-    project: {
+const options = { env: { snapshotOnly: true } }
+
+let issue 
+
+describe('Create Issue', options, () => {
+  beforeEach(() => {
+    issue = {
+      title: `issue-${faker.datatype.uuid()}`,
+      description: faker.random.words(5),
+      project: {
         name: `project-${faker.datatype.uuid()}`,
         description: faker.random.words(5)
+      }
     }
-}
-beforeEach(() => {
-    cy.api_deleteProjects()
-    cy.login()
-     
-})
 
-it('cria novo issue', () => {
-    cy.api_createProject(issue.project)   //CRIA O PROJETO VIA API DENTRO DE UM GUI
+    cy.api_deleteProjects()
+    cy.api_createProject(issue.project)
+    cy.login()
+    cy.visit('/')
+  })
+
+  it('cria novo issue', () => {
     cy.gui_createIssue(issue)
 
     cy.get('.issue-details', { timeout: 10000 }).should('exist')
-        .should('contain', issue.title)
-        .and('contain', issue.description)
-
-
-
-
+      .should('contain', issue.title)
+      .and('contain', issue.description)
+  })
 })
--- cypress/e2e/gui/prejeto.cy.js
+++ cypress/e2e/gui/prejeto.cy.js
@@ -1,29 +1,24 @@
 import { faker } from '@faker-js/faker'
 
-describe('Criar projeto', () => {
-    beforeEach(()=> {
-        cy.api_deleteProjects() //limpa os projetos antes de cada teste
-  
-        cy.login()
-        cy.visit('/')
-        
+const options = { env: { snapshotOnly: true } }
 
-    })
-    it('Cria projeto com sucesso', () => {
+describe('Criar projeto', options, () => {
+  const project = {
+    name: `project-${faker.datatype.uuid()}`,
+    description: faker.random.words(5)
+  }
 
-        const project = {
-            name:`project-${faker.datatype.uuid()}`,
-            description:faker.random.words(5)
-            
-        }
-        
+  beforeEach(() => {
+    cy.api_deleteProjects() // limpa os projetos antes de cada teste
+    cy.login()
+    cy.visit('/')
+  })
 
-        cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`)
-        cy.contains(project.name).should('be.visible')                                                    //fazem as verificacoes por nome e descrição se o projeto foi criado
-        cy.contains(project.description).should('be.visible')
+  it('Cria projeto com sucesso', () => {
+    cy.create_project_gui(project) // cria o projeto via GUI
 
-
-    })
-
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`)
+    cy.contains(project.name).should('be.visible')
+    cy.contains(project.description).should('be.visible')
+  })
 })
-
